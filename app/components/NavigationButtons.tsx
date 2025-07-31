1a93565d4e0fd2c9552503ca4a9588767d56caa1
Sebastian Kullander <sebastian.kullander@protonmail.com>
2025-07-31 14:41:02 +0200

    improved pagination to only show for 4 steps at a time

--- app/components/NavigationButtons.tsx
+++ app/components/NavigationButtons.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { useState, useEffect } from "react";
 
 interface Lesson {
     id: number;
@@ -16,11 +16,29 @@ interface NavigationButtonsProps {
 }
 
 export default function NavigationButtons({ lessons, handlePrev, handleNext, pageIndex, setPageIndex, phase}: NavigationButtonsProps) {
+    const [reducedLessons, setReducedLessons] = useState<Lesson[]>([]);
+    
+    const getCurrentThirdOfLessons = () => {
+        const thirdSize = Math.ceil(lessons.length / 3);
+
+        const currentThirdIndex = Math.floor(pageIndex / thirdSize);
+        const start = currentThirdIndex * thirdSize;
+        const end = start + thirdSize;
+
+        const currentThird = lessons.slice(start, end);
+
+        return currentThird;
+    }
+
+    useEffect(() => {
+        setReducedLessons(getCurrentThirdOfLessons)
+    }, [pageIndex])
+
     return (
         <div className="flex gap-2">
             <button disabled={phase === "start" ? true : false} onClick={handlePrev} className={`py-4 px-8 font-semibold border-1 border-gray-300 rounded-sm ${phase === "start" ? "bg-gray-50 hover:bg-gray-50 text-gray-300 border-gray-200 cursor-not-allowed" : "bg-gray-100 hover:bg-gray-200 text-gray-800 border-gray-300 cursor-pointer"}`}>Prev</button>
-            {lessons.map(({id}) => {
-                return <button key={id} onClick={() => setPageIndex(id)} className={`bg-gray-100 py-4 px-6 font-semibold border-1 border-gray-300 rounded-sm cursor-pointer hover:bg-gray-200 ${id === pageIndex ? "bg-gray-800 text-gray-50 hover:bg-gray-800 border-gray-800" : "text-gray-800"}`}>{id + 1}</button>
+            {reducedLessons.map(({id}) => {
+                return <button key={id} onClick={() => setPageIndex(id)} className={`bg-gray-100 py-4 w-15 font-semibold border-1 border-gray-300 rounded-sm cursor-pointer hover:bg-gray-200 ${id === pageIndex ? "bg-gray-800 text-gray-50 hover:bg-gray-800 border-gray-800" : "text-gray-800"}`}>{id + 1}</button>
             })}
             <button disabled={phase === "end" ? true : false} onClick={handleNext} className={`flex flex-row items-center gap-2 py-4 px-8 font-semibold border-1 rounded-sm  ${phase === "end" ? "bg-gray-50 hover:bg-gray-50 text-gray-300 border-gray-200 cursor-not-allowed" : "bg-gray-100 hover:bg-gray-200 text-gray-800 border-gray-300 cursor-pointer"}`}>Next</button>
         </div>
